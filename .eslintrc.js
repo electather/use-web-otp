df79ecb15fd9742e23972214811cbffffcbf0227
Omid Astaraki <omidocean@gmail.com>
2021-08-30 16:05:11 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['prettier', 'react-app', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        tabWidth: 2,
+        printWidth: 100,
+        semicolons: true,
+        quoteProps: 'as-needed',
+        jsxSingleQuote: false,
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        arrowParens: 'always',
+        endOfLine: 'lf',
+      },
+    ],
+  },
+};
---
+++ src/use-web-otp.ts
@@ -0,0 +1,46 @@
+import { useEffect, useRef } from 'react';
+
+type onSuccessEvent = (otpCode: string) => unknown;
+
+function useWebOtp(onSuccess: onSuccessEvent) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const abortController = useRef(new AbortController());
+
+  useEffect(() => {
+    let isMounted = true;
+    const form = inputRef?.current?.closest('form');
+    const onSubmitEvent = () => {
+      abortController.current.abort();
+    };
+    if (form) {
+      form.addEventListener('submit', onSubmitEvent);
+    }
+    (navigator.credentials as any) // new WebOTP API types are not added yet
+      .get({
+        otp: { transport: ['sms'] },
+        signal: abortController.current.signal,
+      })
+      .then((otp: any) => {
+        if (!isMounted) {
+          return;
+        }
+        if (inputRef.current) {
+          inputRef.current.value ??= otp.code;
+        }
+        if (form) {
+          form.submit();
+        }
+        onSuccess?.(otp.code);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    return () => {
+      form?.removeEventListener('submit', onSubmitEvent);
+      isMounted = false;
+    };
+  }, []);
+}
+
+export default useWebOtp;
